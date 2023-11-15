fbe3bb06d750152b2a463968dc0c142bb948b8e6
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-11-15 18:22:56 +0900

    feature: 좌표기반 관광지 캐시 데이터에서 찾기

--- GTA_SPRING/src/main/java/com/jsj/GTA/util/Coordinate.java
+++ GTA_SPRING/src/main/java/com/jsj/GTA/util/Coordinate.java
@@ -1,6 +1,7 @@
 package com.jsj.GTA.util;
 
 import com.jsj.GTA.api.touristAttractions.TouristAttractionsListResponseDto;
+import com.jsj.GTA.api.touristAttractions.redis.TouristAttractionsResponseRedisDto;
 import lombok.NoArgsConstructor;
 
 import java.util.Comparator;
@@ -11,6 +12,21 @@ import java.util.stream.Collectors;
 public class Coordinate {
 
     public static List<TouristAttractionsListResponseDto> findClosestCoordinates(List<TouristAttractionsListResponseDto> coordinates, int count, double lat, double lng) {
+        if (coordinates.size() == 0 || count <= 0) {
+            // 좌표 리스트가 비어 있거나 count가 0 이하일 경우 빈 리스트 반환
+            return List.of();
+        }
+
+        return coordinates.stream()
+                .sorted(Comparator.comparingDouble(coordinate -> {
+                    double listLat = Double.parseDouble(coordinate.getLat());
+                    double listLng = Double.parseDouble(coordinate.getLng());
+                    return Math.sqrt(Math.pow(listLat - lat, 2) + Math.pow(listLng - lng, 2));
+                }))
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+    public static List<TouristAttractionsResponseRedisDto> findClosestCoordinatesWithRedis(List<TouristAttractionsResponseRedisDto> coordinates, int count, double lat, double lng) {
         if (coordinates.size() == 0 || count <= 0) {
             // 좌표 리스트가 비어 있거나 count가 0 이하일 경우 빈 리스트 반환
             return List.of();
