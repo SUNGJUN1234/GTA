d034b49fe84dc3043efc37d151a5259bc0b29c20
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2024-01-14 20:10:23 +0900

    로그아웃, 회원탈퇴

--- GTA_SPRING/src/main/java/com/jsj/GTA/service/UsersService.java
+++ GTA_SPRING/src/main/java/com/jsj/GTA/service/UsersService.java
@@ -26,17 +26,19 @@ public class UsersService implements UserDetailsService {
     private final TokenService tokenService;
     private final AuthService authService;
 
-    private final Logger LOGGER = LoggerFactory.getLogger(TouristAttractionsService.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(UserDetailsService.class);
 
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        LOGGER.info("[loadUserByUsername] request data: {}", userId);
         return usersRepository.findByUserId(userId)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("userId: " + userId + "를 데이터베이스에서 찾을 수 없습니다."));
     }
 
     private UserDetails createUserDetails(Users users) {
+        LOGGER.info("[createUserDetails] request data: {}", users.toString());
         GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(users.getRole().getKey());
 
         return new User(
@@ -52,6 +54,7 @@ public class UsersService implements UserDetailsService {
     }
 
     public UsersDto findByUserId(String userId) {
+        LOGGER.info("[findByUserId] request data: {}", userId);
         Users entity = usersRepository.findByUserId(userId).orElseThrow(() -> new RuntimeException("해당 id를 가진 사용자가 존재하지 않습니다."));
         return new UsersDto(entity);
     }
@@ -69,13 +72,13 @@ public class UsersService implements UserDetailsService {
      */
     @Transactional
     public TokenDto login(LoginRequestDto requestDto) {
-        LOGGER.info("UsersService[login] requestDto data: {}", requestDto);
+        LOGGER.info("[login] requestDto data: {}", requestDto.toString());
         authService.authenticateLogin(requestDto);
 
         Users users = usersRepository.findByUserId(requestDto.getUserId()).get();
-        LOGGER.info("UsersService[login] findByUserId data userId: {}", users.getUserId());
+        LOGGER.info("[login] findByUserId data userId: {}", users.getUserId());
         TokenDto tokenDto = tokenService.createToken(users);
-        LOGGER.info("UsersService[login] tokenDto TokenType: {} AccessToken: {} RefreshToken: {} Duration: {}",
+        LOGGER.info("[login] tokenDto TokenType: {} AccessToken: {} RefreshToken: {} Duration: {}",
                 tokenDto.getTokenType(),
                 tokenDto.getAccessToken(),
                 tokenDto.getRefreshToken(),
@@ -85,18 +88,32 @@ public class UsersService implements UserDetailsService {
 
     @Transactional
     public void signup(UsersRequestDto requestDto) {
-        LOGGER.info("UsersService[signup] requestDTO data: {}", requestDto);
+        LOGGER.info("[signup] requestDTO data: {}", requestDto.toString());
         Optional<Users> entity = usersRepository.findByUserId(requestDto.getUserId());
         if(entity.isPresent()) {
-            LOGGER.info("UsersService[signup] already exist data, entity: {}", entity.get().getUserId());
+            LOGGER.info("[signup] already exist data, entity: {}", entity.get().getUserId());
             throw new RuntimeException("이미 존재하는 아이디입니다.");
         } else {
             Users users = requestDto.toEntity(requestDto);
-            LOGGER.info("UsersService[signup] new users data: {}", users.getUserId());
+            LOGGER.info("[signup] new users data: {}", users.getUserId());
             usersRepository.save(users);
         }
 //        users.updateRole(Role.USER);
 //        usersRepository.save(users);
     }
 
+    @Transactional
+    public void withdraw(LoginRequestDto requestDto) {
+        LOGGER.info("[withdraw] requestDto data: {}", requestDto.toString());
+        authService.authenticateLogin(requestDto);
+
+        Optional<Users> users = usersRepository.findByUserId(requestDto.getUserId());
+        LOGGER.info("[withdraw] findByUserId data userId: {}", users.get().getUserId());
+        if(users.isPresent()) {
+            Users user = users.get();
+            user.delete();
+            LOGGER.info("[withdraw] users status: {}", user.getStatus());
+            usersRepository.save(user);
+        }
+    }
 }
